3b9a97ac2040c95c155f12312c7452960df51503
Linus Torvalds <me@theholytachanka.com>
2023-10-30 15:55:49 +0200

    Format create.c

--- src/create.c
+++ src/create.c
@@ -1,12 +1,12 @@
-#include <stdio.h>         // Standard I/O library for file operations
-#include <stdlib.h>        // Standard library for general functions
-#include <string.h>        // Standard library for string manipulation
+#include <stdio.h>  // Standard I/O library for file operations
+#include <stdlib.h> // Standard library for general functions
+#include <string.h> // Standard library for string manipulation
 
-#include "libspm.h"        // Custom library for package management
-#include "cutils.h"        // Custom utility library
+#include "cutils.h" // Custom utility library
+#include "libspm.h" // Custom library for package management
 
 // Function prototype for creating an archive from a directory
-int create_archive(const char* DIR, const char* out_path);
+int create_archive(const char *DIR, const char *out_path);
 
 // Function to create a binary package from source
 /*
@@ -17,8 +17,9 @@ Accepts:
 Returns:
 - int: An integer indicating the result of the binary package creation.
 */
-int create_binary_from_source(const char* spm_path, const char* bin_path) {
-    return f_create_binary_from_source(spm_path, bin_path, NULL, getenv("SOVIET_DEFAULT_FORMAT"));
+int create_binary_from_source(const char *spm_path, const char *bin_path) {
+  return f_create_binary_from_source(spm_path, bin_path, NULL,
+                                     getenv("SOVIET_DEFAULT_FORMAT"));
 }
 
 // Function to create a binary package from source with input and output formats
@@ -32,57 +33,59 @@ Accepts:
 Returns:
 - int: An integer indicating the result of the binary package creation.
 */
-int f_create_binary_from_source(const char* src_path, const char* bin_path, const char* in_format, const char* out_format)
-{
-    struct package pkg;
+int f_create_binary_from_source(const char *src_path, const char *bin_path,
+                                const char *in_format, const char *out_format) {
+  struct package pkg;
 
-    // Open the package source
-    open_pkg(src_path, &pkg, NULL);
+  // Open the package source
+  open_pkg(src_path, &pkg, NULL);
 
-    PACKAGE_QUEUE[QUEUE_COUNT] = pkg.name; // Add the package name to the PKG_QUEUE ARRAY
-    QUEUE_COUNT++;
-    dbg(1, "Added %s at QUEUE[%d] ", pkg.name, QUEUE_COUNT);
+  PACKAGE_QUEUE[QUEUE_COUNT] =
+      pkg.name; // Add the package name to the PKG_QUEUE ARRAY
+  QUEUE_COUNT++;
+  dbg(1, "Added %s at QUEUE[%d] ", pkg.name, QUEUE_COUNT);
 
-    /* 
-        Here we have some problems:
-        The legacy package directory was in MAKE_DIR/$NAME-$VERSION
-        Should we keep it or not? 
-        I choose, for compatibility reasons, to keep it.
-        If someone wants to change this, you can vote here:
-         - keep it: 1
-         - change it: 0
-    */
-    const char* MAKE_DIR = getenv("SOVIET_MAKE_DIR");
-    const char* BUILD_DIR = getenv("SOVIET_BUILD_DIR");
+  /*
+      Here we have some problems:
+      The legacy package directory was in MAKE_DIR/$NAME-$VERSION
+      Should we keep it or not?
+      I choose, for compatibility reasons, to keep it.
+      If someone wants to change this, you can vote here:
+       - keep it: 1
+       - change it: 0
+  */
+  const char *MAKE_DIR = getenv("SOVIET_MAKE_DIR");
+  const char *BUILD_DIR = getenv("SOVIET_BUILD_DIR");
 
-    char legacy_dir[MAX_PATH];
-    sprintf(legacy_dir, "%s/%s-%s", MAKE_DIR, pkg.name, pkg.version);
-    dbg(1, "legacy dir: %s", legacy_dir);
+  char legacy_dir[MAX_PATH];
+  sprintf(legacy_dir, "%s/%s-%s", MAKE_DIR, pkg.name, pkg.version);
+  dbg(1, "legacy dir: %s", legacy_dir);
 
-    // Make the package
-    if (make(legacy_dir, &pkg) != 0) {
-        msg(FATAL, "Make failed");
-    }
-    dbg(1, "Make done - %s", pkg.name);
+  // Make the package
+  if (make(legacy_dir, &pkg) != 0) {
+    msg(FATAL, "Make failed");
+  }
+  dbg(1, "Make done - %s", pkg.name);
 
-    // Get package locations
-    dbg(1, "Getting locations - %s", pkg.name);
-    pkg.locationsCount = get_locations(&pkg.locations, BUILD_DIR);
+  // Get package locations
+  dbg(1, "Getting locations - %s", pkg.name);
+  pkg.locationsCount = get_locations(&pkg.locations, BUILD_DIR);
 
-    char file_path[MAX_PATH];
-    sprintf(file_path, "%s/%s.%s", BUILD_DIR, pkg.name, getenv("SOVIET_DEFAULT_FORMAT"));
+  char file_path[MAX_PATH];
+  sprintf(file_path, "%s/%s.%s", BUILD_DIR, pkg.name,
+          getenv("SOVIET_DEFAULT_FORMAT"));
 
-    // Create a package file
-    create_pkg(file_path, &pkg, NULL);
+  // Create a package file
+  create_pkg(file_path, &pkg, NULL);
 
-    // Compress binaries to a package archive
-    dbg(1, "Compressing binaries - %s", pkg.name);
-    create_archive(BUILD_DIR, bin_path);
+  // Compress binaries to a package archive
+  dbg(1, "Compressing binaries - %s", pkg.name);
+  create_archive(BUILD_DIR, bin_path);
 
-    // Clean working directories
-    clean();
+  // Clean working directories
+  clean();
 
-    return 0;
+  return 0;
 }
 
 // Function to create an archive from a directory
@@ -94,12 +97,11 @@ Accepts:
 Returns:
 - int: An integer indicating the result of the archive creation.
 */
-int create_archive(const char* DIR, const char* out_path)
-{
-    char* archive_cmd = calloc(256, sizeof(char));
-    sprintf(archive_cmd, "( cd %s && tar -czf %s . )", DIR, out_path);
-    dbg(1, "archive_cmd: %s", archive_cmd);
-    int EXIT = system(archive_cmd);
-    free(archive_cmd);
-    return EXIT;
+int create_archive(const char *DIR, const char *out_path) {
+  char *archive_cmd = calloc(256, sizeof(char));
+  sprintf(archive_cmd, "( cd %s && tar -czf %s . )", DIR, out_path);
+  dbg(1, "archive_cmd: %s", archive_cmd);
+  int EXIT = system(archive_cmd);
+  free(archive_cmd);
+  return EXIT;
 }
