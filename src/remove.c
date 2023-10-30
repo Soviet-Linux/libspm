6f0da7f193aa4308ab20fddc0f39cfeaaca267e2
Linus Torvalds <me@theholytachanka.com>
2023-10-30 15:58:56 +0200

    format remove.c

--- src/remove.c
+++ src/remove.c
@@ -1,6 +1,6 @@
 #define _XOPEN_SOURCE 500
-#include <stdio.h>
 #include <ftw.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #include "libspm.h"
@@ -9,9 +9,11 @@
 /*
 Accepts:
 - const char *fpath: The path of the file or directory being processed.
-- const struct stat *sb: A pointer to a structure containing information about the file.
+- const struct stat *sb: A pointer to a structure containing information about
+the file.
 - int typeflag: A flag indicating the type of the file (file, directory, etc.).
-- struct FTW *ftwbuf: A pointer to a structure containing state information for the traversal.
+- struct FTW *ftwbuf: A pointer to a structure containing state information for
+the traversal.
 
 Returns:
 - int: An integer indicating the result of the operation.
@@ -19,16 +21,20 @@ Returns:
   - Non-zero: An error occurred during the operation.
 
 Description:
-This function is used as a callback by the nftw function to unlink (remove) files and directories. It attempts to remove the file or directory specified by 'fpath' and returns 0 if the removal is successful. If an error occurs during the removal, it returns a non-zero value and prints an error message indicating the file or directory that caused the error.
+This function is used as a callback by the nftw function to unlink (remove)
+files and directories. It attempts to remove the file or directory specified by
+'fpath' and returns 0 if the removal is successful. If an error occurs during
+the removal, it returns a non-zero value and prints an error message indicating
+the file or directory that caused the error.
 */
-int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
-{
-    int rv = remove(fpath);
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag,
+              struct FTW *ftwbuf) {
+  int rv = remove(fpath);
 
-    if (rv)
-        perror(fpath);
+  if (rv)
+    perror(fpath);
 
-    return rv;
+  return rv;
 }
 
 // Recursively remove a directory and its contents
@@ -42,9 +48,10 @@ Returns:
   - Non-zero: An error occurred during the removal.
 
 Description:
-This function recursively removes a directory and its contents. It utilizes the nftw function to traverse the directory and its subdirectories, invoking the 'unlink_cb' callback function to unlink and remove files and directories. It returns 0 if the removal is successful and a non-zero value if an error occurs during the operation.
+This function recursively removes a directory and its contents. It utilizes the
+nftw function to traverse the directory and its subdirectories, invoking the
+'unlink_cb' callback function to unlink and remove files and directories. It
+returns 0 if the removal is successful and a non-zero value if an error occurs
+during the operation.
 */
-int rmrf(char *path)
-{
-    return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
-}
+int rmrf(char *path) { return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS); }
