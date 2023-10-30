a99e50c6ef855cdd2012340b444460e4ae98152c
Linus Torvalds <me@theholytachanka.com>
2023-10-30 15:58:11 +0200

    Format make.c

--- src/make.c
+++ src/make.c
@@ -4,9 +4,9 @@
 #include <sys/stat.h>
 
 // Include necessary headers
-#include "libspm.h"
 #include "cutils.h"
 #include "globals.h"
+#include "libspm.h"
 
 // Function to build and install a package
 /*
@@ -17,101 +17,111 @@ Accepts:
 Returns:
 - int: An integer indicating the result of the build and installation process.
   - 0: Build and installation succeeded.
-  - 1: An error occurred during execution of prepare, make, test, or install commands.
+  - 1: An error occurred during execution of prepare, make, test, or install
+commands.
   - -2: Failed to install the package.
   - -3: No install command found.
 */
-int make(char* package_dir, struct package* pkg) {
-    char* build_dir = getenv("SOVIET_BUILD_DIR");
-    char* make_dir = getenv("SOVIET_MAKE_DIR");
+int make(char *package_dir, struct package *pkg) {
+  char *build_dir = getenv("SOVIET_BUILD_DIR");
+  char *make_dir = getenv("SOVIET_MAKE_DIR");
 
-    char* cmd_params;
-    if (QUIET) { 
-        cmd_params = "&> /dev/null"; 
-    } else { 
-        cmd_params = ""; 
+  char *cmd_params;
+  if (QUIET) {
+    cmd_params = "&> /dev/null";
+  } else {
+    cmd_params = "";
+  }
+
+  // Set environment variables for building
+  setenv("BUILD_ROOT", build_dir, 1);
+  setenv("NAME", pkg->name, 1);
+  setenv("VERSION", pkg->version, 1);
+
+  // Extract URL from the package
+  char excmd[64 + strlen(pkg->url)];
+  sprintf(excmd, "echo -n %s", pkg->url);
+  char *exurl = exec(excmd);
+  setenv("URL", exurl, 1);
+  free(exurl);
+
+  // Download package sources
+  if (pkg->info.download != NULL && strlen(pkg->info.download) > 0) {
+    char sources_cmd[64 + strlen(make_dir) + strlen(pkg->info.download)];
+
+    sprintf(sources_cmd, "(cd %s && %s) %s ", make_dir, pkg->info.download,
+            cmd_params);
+    dbg(2, "Downloading sources with %s", sources_cmd);
+    int res = system(sources_cmd);
+
+    if (res != 0) {
+      msg(ERROR, "Failed to download sources for %s", pkg->name);
+      return -1;
     }
+  }
 
-    // Set environment variables for building
-    setenv("BUILD_ROOT", build_dir, 1);
-    setenv("NAME", pkg->name, 1);
-    setenv("VERSION", pkg->version, 1);
+  // Run 'prepare' command
+  if (pkg->info.prepare != NULL && strlen(pkg->info.prepare) > 0) {
+    char prepare_cmd[64 + strlen(package_dir) + strlen(pkg->info.prepare) +
+                     strlen(cmd_params)];
 
-    // Extract URL from the package
-    char excmd[64 + strlen(pkg->url)];
-    sprintf(excmd, "echo -n %s", pkg->url);
-    char* exurl = exec(excmd);
-    setenv("URL", exurl, 1);
-    free(exurl);
+    sprintf(prepare_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.prepare,
+            cmd_params);
 
-    // Download package sources
-    if (pkg->info.download != NULL && strlen(pkg->info.download) > 0) {
-        char sources_cmd[64 + strlen(make_dir) + strlen(pkg->info.download)];
-
-        sprintf(sources_cmd, "(cd %s && %s) %s ", make_dir, pkg->info.download, cmd_params);
-        dbg(2, "Downloading sources with %s", sources_cmd);
-        int res = system(sources_cmd);
-
-        if (res != 0) {
-            msg(ERROR, "Failed to download sources for %s", pkg->name);
-            return -1;
-        }
+    dbg(2, "Executing prepare command: %s", prepare_cmd);
+    if (system(prepare_cmd) != 0) {
+      return 1;
     }
+    dbg(1, "Prepare command executed!");
+  }
 
-    // Run 'prepare' command
-    if (pkg->info.prepare != NULL && strlen(pkg->info.prepare) > 0) {
-        char prepare_cmd[64 + strlen(package_dir) + strlen(pkg->info.prepare) + strlen(cmd_params)];
+  // Run 'make' command
+  if (pkg->info.make && strlen(pkg->info.make)) {
+    char make_cmd[64 + strlen(package_dir) + strlen(pkg->info.make) +
+                  strlen(cmd_params)];
+    sprintf(make_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.make,
+            cmd_params);
 
-        sprintf(prepare_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.prepare, cmd_params);
-
-        dbg(2, "Executing prepare command: %s", prepare_cmd);
-        if (system(prepare_cmd) != 0) {
-            return 1;
-        }
-        dbg(1, "Prepare command executed!");
+    dbg(2, "Executing make command: %s", make_cmd);
+    if (system(make_cmd) != 0) {
+      return 1;
     }
+    dbg(1, "Make command executed!");
+  }
 
-    // Run 'make' command
-    if (pkg->info.make && strlen(pkg->info.make)) {
-        char make_cmd[64 + strlen(package_dir) + strlen(pkg->info.make) + strlen(cmd_params)];
-        sprintf(make_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.make, cmd_params);
+  // Run 'test' command (if in testing mode)
+  if (pkg->info.test != NULL && TESTING && strlen(pkg->info.test) > 0) {
+    char test_cmd[64 + strlen(package_dir) + strlen(pkg->info.test) +
+                  strlen(cmd_params)];
+    sprintf(test_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.test,
+            cmd_params);
 
-        dbg(2, "Executing make command: %s", make_cmd);
-        if (system(make_cmd) != 0) {
-            return 1;
-        }
-        dbg(1, "Make command executed!");
+    dbg(2, "Executing test command: %s", test_cmd);
+    if (system(test_cmd) != 0) {
+      return 1;
     }
+    dbg(1, "Test command executed!");
+  }
 
-    // Run 'test' command (if in testing mode)
-    if (pkg->info.test != NULL && TESTING && strlen(pkg->info.test) > 0) {
-        char test_cmd[64 +  strlen(package_dir) + strlen(pkg->info.test) + strlen(cmd_params)];
-        sprintf(test_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.test, cmd_params);
+  // Run 'install' command
+  if (pkg->info.install == NULL && strlen(pkg->info.install) == 0) {
+    msg(ERROR, "No install command!");
+    return -3;
+  }
 
-        dbg(2, "Executing test command: %s", test_cmd);
-        if (system(test_cmd) != 0) {
-            return 1;
-        }
-        dbg(1, "Test command executed!");
-    }
+  char install_cmd[64 + strlen(package_dir) + strlen(pkg->info.install) +
+                   strlen(cmd_params)];
+  sprintf(install_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.install,
+          cmd_params);
 
-    // Run 'install' command
-    if (pkg->info.install == NULL && strlen(pkg->info.install) == 0) {
-        msg(ERROR, "No install command!");
-        return -3;
-    }
+  dbg(2, "Executing install command: %s", install_cmd);
+  if (system(install_cmd) != 0) {
+    msg(FATAL, "Failed to install %s", pkg->name);
+    return -2;
+  }
+  dbg(1, "Install command executed!");
 
-    char install_cmd[64 + strlen(package_dir) + strlen(pkg->info.install) + strlen(cmd_params)];
-    sprintf(install_cmd, "( cd %s && %s ) %s", package_dir, pkg->info.install, cmd_params);
-
-    dbg(2, "Executing install command: %s", install_cmd);
-    if (system(install_cmd) != 0) {
-        msg(FATAL, "Failed to install %s", pkg->name);
-        return -2;
-    }
-    dbg(1, "Install command executed!");
-
-    return 0;
+  return 0;
 }
 
 // Function to execute a special command for post-installation
@@ -125,15 +135,16 @@ Returns:
   - 0: Special command executed successfully.
   - 1: An error occurred during special command execution.
 */
-int exec_special(const char* cmd, const char* package_dir) {
-    dbg(2, "Executing special command: %s");
-    char* special_cmd = calloc(64 + strlen(package_dir) + strlen(cmd), sizeof(char));
+int exec_special(const char *cmd, const char *package_dir) {
+  dbg(2, "Executing special command: %s");
+  char *special_cmd =
+      calloc(64 + strlen(package_dir) + strlen(cmd), sizeof(char));
 
-    if (system(special_cmd) != 0) {
-        free(special_cmd);
-        return 1;
-    }
+  if (system(special_cmd) != 0) {
     free(special_cmd);
-    dbg(1, "Special command executed!");
-    return 0;
+    return 1;
+  }
+  free(special_cmd);
+  dbg(1, "Special command executed!");
+  return 0;
 }
