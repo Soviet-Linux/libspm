326bab846c929767717225769563d92aaf02ef88
Linus Torvalds <me@theholytachanka.com>
2023-10-30 16:00:34 +0200

    Format hashtable.h

--- include/hashtable.h
+++ include/hashtable.h
@@ -1,26 +1,26 @@
 typedef struct pair {
-    char* key;
-    void* value;
+  char *key;
+  void *value;
 } pair;
 
 typedef struct {
-    pair* data;
-    int size;
-    int capacity;
+  pair *data;
+  int size;
+  int capacity;
 } item;
 
 typedef struct {
-    item *items;
-    int capacity;
+  item *items;
+  int capacity;
 } hashtable;
-
 
 hashtable *hm_create(int capacity);
 void hm_destroy(hashtable *hm);
 int hm_add(hashtable *hm, char *key, void *value);
-void* hm_get(hashtable *hm, char *key);
+void *hm_get(hashtable *hm, char *key);
 int hm_rm(hashtable *hm, char *key);
 int hm_visualize(hashtable *hm);
-hashtable* hm_init(void* kvlist[][2],int size);
+hashtable *hm_init(void *kvlist[][2], int size);
 
-unsigned int hm_hash(hashtable *hm, char *key);;
+unsigned int hm_hash(hashtable *hm, char *key);
+;
