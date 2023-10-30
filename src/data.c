e3d88fce63790e3b8ed736abb5194b91e64633a6
Linus Torvalds <me@theholytachanka.com>
2023-10-30 15:56:03 +0200

    format data.c

--- src/data.c
+++ src/data.c
@@ -1,10 +1,10 @@
-#include "string.h"        // Standard library for string manipulation
-#include "unistd.h"        // Standard library for system calls
-#include "sqlite3.h"      // SQLite database library
-#include "stdio.h"         // Standard I/O library for file operations
+#include "sqlite3.h" // SQLite database library
+#include "stdio.h"   // Standard I/O library for file operations
+#include "string.h"  // Standard library for string manipulation
+#include "unistd.h"  // Standard library for system calls
 
-#include "libspm.h"        // Custom library for package management
-#include "cutils.h"        // Custom utility library
+#include "cutils.h" // Custom utility library
+#include "libspm.h" // Custom library for package management
 
 // Callback function for SQLite queries
 int callback(void *NotUsed, int argc, char **argv, char **azColName);
@@ -21,13 +21,13 @@ Returns:
   - 1: Failure, unable to open the database.
 */
 int connect_db(sqlite3 **db, char *DB_PATH) {
-    int rc = sqlite3_open(DB_PATH, db);
-    if (rc) {
-        msg(ERROR, "Can't open database: %s\n", sqlite3_errmsg(*db));
-        sqlite3_close(*db);
-        return 1;
-    }
-    return 0;
+  int rc = sqlite3_open(DB_PATH, db);
+  if (rc) {
+    msg(ERROR, "Can't open database: %s\n", sqlite3_errmsg(*db));
+    sqlite3_close(*db);
+    return 1;
+  }
+  return 0;
 }
 
 // Function to create an SQLite table for installed packages
@@ -41,16 +41,19 @@ Returns:
   - 1: Failure, SQL error.
 */
 int create_table_installed(sqlite3 *db) {
-    char *err_msg = NULL;
-    int rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS Packages (Name TEXT, Version TEXT, Type TEXT, AsDep INT)", 0, 0, &err_msg);
+  char *err_msg = NULL;
+  int rc = sqlite3_exec(db,
+                        "CREATE TABLE IF NOT EXISTS Packages (Name TEXT, "
+                        "Version TEXT, Type TEXT, AsDep INT)",
+                        0, 0, &err_msg);
 
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-        return 1;
-    }
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "SQL error: %s\n", err_msg);
+    sqlite3_free(err_msg);
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }
 
 // Function to store data for an installed package in the database
@@ -58,7 +61,8 @@ int create_table_installed(sqlite3 *db) {
 Accepts:
 - sqlite3 *db: An SQLite database handle.
 - struct package *pkg: A pointer to a package structure.
-- int as_dep: Flag indicating whether the package is installed as a dependency (1 for yes, 0 for no).
+- int as_dep: Flag indicating whether the package is installed as a dependency
+(1 for yes, 0 for no).
 
 Returns:
 - int: An integer indicating the result of data storage.
@@ -66,42 +70,42 @@ Returns:
   - -1: Failure, SQL error.
 */
 int store_data_installed(sqlite3 *db, struct package *pkg, int as_dep) {
-    sqlite3_stmt *stmt;  // Statement handle.
-    int rc;              // Return code.
+  sqlite3_stmt *stmt; // Statement handle.
+  int rc;             // Return code.
 
-    // Prepare the INSERT statement.
-    rc = sqlite3_prepare_v2(db,
-                            "INSERT INTO Packages (Name, Version, Type, AsDep) VALUES (?, ?, ?, ?)",
-                            -1,
-                            &stmt,
-                            NULL);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "Error preparing INSERT statement: %s\n", sqlite3_errmsg(db));
-        return -1;
-    }
+  // Prepare the INSERT statement.
+  rc = sqlite3_prepare_v2(
+      db,
+      "INSERT INTO Packages (Name, Version, Type, AsDep) VALUES (?, ?, ?, ?)",
+      -1, &stmt, NULL);
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "Error preparing INSERT statement: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
 
-    // Bind the values to the INSERT statement.
-    sqlite3_bind_text(stmt, 1, pkg->name, -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 2, pkg->version, -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 3, pkg->type, -1, SQLITE_STATIC);
-    sqlite3_bind_int(stmt, 4, as_dep);
+  // Bind the values to the INSERT statement.
+  sqlite3_bind_text(stmt, 1, pkg->name, -1, SQLITE_STATIC);
+  sqlite3_bind_text(stmt, 2, pkg->version, -1, SQLITE_STATIC);
+  sqlite3_bind_text(stmt, 3, pkg->type, -1, SQLITE_STATIC);
+  sqlite3_bind_int(stmt, 4, as_dep);
 
-    // Execute the INSERT statement.
-    rc = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
-        msg(ERROR, "Error executing INSERT statement: %s\n", sqlite3_errmsg(db));
-        return -1;
-    }
-    dbg(1, "Inserted %s %s %s -- exit: %d ", pkg->name, pkg->version, pkg->type, rc);
+  // Execute the INSERT statement.
+  rc = sqlite3_step(stmt);
+  if (rc != SQLITE_DONE) {
+    msg(ERROR, "Error executing INSERT statement: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
+  dbg(1, "Inserted %s %s %s -- exit: %d ", pkg->name, pkg->version, pkg->type,
+      rc);
 
-    // Finalize the statement.
-    rc = sqlite3_finalize(stmt);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "Error finalizing INSERT statement: %s\n", sqlite3_errmsg(db));
-        return -1;
-    }
+  // Finalize the statement.
+  rc = sqlite3_finalize(stmt);
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "Error finalizing INSERT statement: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 // Function to retrieve data for an installed package from the database
@@ -109,7 +113,8 @@ int store_data_installed(sqlite3 *db, struct package *pkg, int as_dep) {
 Accepts:
 - sqlite3 *db: An SQLite database handle.
 - struct package *pkg: A pointer to a package structure.
-- int *as_dep: A pointer to an integer to store whether the package was installed as a dependency (optional).
+- int *as_dep: A pointer to an integer to store whether the package was
+installed as a dependency (optional).
 
 Returns:
 - int: An integer indicating the result of data retrieval.
@@ -117,35 +122,36 @@ Returns:
   - -1: Failure, SQL error.
 */
 int retrieve_data_installed(sqlite3 *db, struct package *pkg, int *as_dep) {
-    sqlite3_stmt *stmt;
-    int rc;
+  sqlite3_stmt *stmt;
+  int rc;
 
-    dbg(1, "Retrieving %s data from DB", pkg->name);
+  dbg(1, "Retrieving %s data from DB", pkg->name);
 
-    // Prepare the SQL query
-    const char *sql = "SELECT Version, Type, AsDep FROM Packages WHERE Name = ?";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "SQL error: %s -- %d\n", sqlite3_errmsg(db), rc);
-        return -1;
-    }
+  // Prepare the SQL query
+  const char *sql = "SELECT Version, Type, AsDep FROM Packages WHERE Name = ?";
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "SQL error: %s -- %d\n", sqlite3_errmsg(db), rc);
+    return -1;
+  }
 
-    // Bind the value for the parameter in the SQL query
-    sqlite3_bind_text(stmt, 1, pkg->name, -1, SQLITE_STATIC);
+  // Bind the value for the parameter in the SQL query
+  sqlite3_bind_text(stmt, 1, pkg->name, -1, SQLITE_STATIC);
 
-    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        pkg->version = strdup((char *)sqlite3_column_text(stmt, 0));
-        pkg->type = strdup((char *)sqlite3_column_text(stmt, 1));
-        if (as_dep != NULL) *as_dep = sqlite3_column_int(stmt, 2);
-    }
+  while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+    pkg->version = strdup((char *)sqlite3_column_text(stmt, 0));
+    pkg->type = strdup((char *)sqlite3_column_text(stmt, 1));
+    if (as_dep != NULL)
+      *as_dep = sqlite3_column_int(stmt, 2);
+  }
 
-    // Check if the SQL query was successful
-    if (rc != SQLITE_DONE) {
-        msg(ERROR, "SQL error: %s\n", sqlite3_errmsg(db));
-        return -1;
-    }
+  // Check if the SQL query was successful
+  if (rc != SQLITE_DONE) {
+    msg(ERROR, "SQL error: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 // Function to remove data for an installed package from the database
@@ -160,35 +166,35 @@ Returns:
   - 1: Failure, SQL error.
 */
 int remove_data_installed(sqlite3 *db, char *name) {
-    sqlite3_stmt *stmt;
-    int rc;
+  sqlite3_stmt *stmt;
+  int rc;
 
-    // Prepare the SQL query
-    const char *sql = "DELETE FROM Packages WHERE Name = ?";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "SQL error: %s -- %d", sqlite3_errmsg(db), rc);
-        return 1;
-    }
+  // Prepare the SQL query
+  const char *sql = "DELETE FROM Packages WHERE Name = ?";
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "SQL error: %s -- %d", sqlite3_errmsg(db), rc);
+    return 1;
+  }
 
-    // Bind the value for the parameter in the SQL query
-    sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
+  // Bind the value for the parameter in the SQL query
+  sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
 
-    // Execute the DELETE statement.
-    rc = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
-        msg(ERROR, "Error executing DELETE statement: %s\n", sqlite3_errmsg(db));
-        return -1;
-    }
+  // Execute the DELETE statement.
+  rc = sqlite3_step(stmt);
+  if (rc != SQLITE_DONE) {
+    msg(ERROR, "Error executing DELETE statement: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
 
-    // Finalize the statement.
-    rc = sqlite3_finalize(stmt);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "Error finalizing DELETE statement: %s\n", sqlite3_errmsg(db));
-        return -1;
-    }
+  // Finalize the statement.
+  rc = sqlite3_finalize(stmt);
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "Error finalizing DELETE statement: %s\n", sqlite3_errmsg(db));
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 // Function to retrieve data for a package from the repository database
@@ -204,37 +210,41 @@ Returns:
   - 0: Success.
   - 1: Failure, SQL error.
 */
-int retrieve_data_repo(sqlite3 *db, struct package *pkg, char **format, char **section) {
-    sqlite3_stmt *stmt;
-    int rc;
+int retrieve_data_repo(sqlite3 *db, struct package *pkg, char **format,
+                       char **section) {
+  sqlite3_stmt *stmt;
+  int rc;
 
-    dbg(1, "Retrieving %s data from repo DB", pkg->name);
+  dbg(1, "Retrieving %s data from repo DB", pkg->name);
 
-    // Prepare the SQL query
-    const char *sql = "SELECT Version, Type, Format, Section FROM Packages WHERE Name = ?";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "SQL error: %s", sqlite3_errmsg(db));
-        return 1;
-    }
+  // Prepare the SQL query
+  const char *sql =
+      "SELECT Version, Type, Format, Section FROM Packages WHERE Name = ?";
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "SQL error: %s", sqlite3_errmsg(db));
+    return 1;
+  }
 
-    // Bind the value for the parameter in the SQL query
-    sqlite3_bind_text(stmt, 1, pkg->name, -1, SQLITE_STATIC);
+  // Bind the value for the parameter in the SQL query
+  sqlite3_bind_text(stmt, 1, pkg->name, -1, SQLITE_STATIC);
 
-    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        pkg->version = strdup((char *)sqlite3_column_text(stmt, 0));
-        pkg->type = strdup((char *)sqlite3_column_text(stmt, 1));
-        if (format != NULL) (*format) = strdup((char *)sqlite3_column_text(stmt, 2));
-        if (section != NULL) (*section) = strdup((char *)sqlite3_column_text(stmt, 3));
-    }
+  while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+    pkg->version = strdup((char *)sqlite3_column_text(stmt, 0));
+    pkg->type = strdup((char *)sqlite3_column_text(stmt, 1));
+    if (format != NULL)
+      (*format) = strdup((char *)sqlite3_column_text(stmt, 2));
+    if (section != NULL)
+      (*section) = strdup((char *)sqlite3_column_text(stmt, 3));
+  }
 
-    // Check if the SQL query was successful
-    if (rc != SQLITE_DONE) {
-        msg(ERROR, "SQL error: %s", sqlite3_errmsg(db));
-        return -1;
-    }
+  // Check if the SQL query was successful
+  if (rc != SQLITE_DONE) {
+    msg(ERROR, "SQL error: %s", sqlite3_errmsg(db));
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 // Function to print all data from the database
@@ -248,30 +258,31 @@ Returns:
   - 1: Failure, SQL error.
 */
 int print_all_data(sqlite3 *db) {
-    sqlite3_stmt *stmt;
-    char *zErrMsg = 0;
-    int rc;
+  sqlite3_stmt *stmt;
+  char *zErrMsg = 0;
+  int rc;
 
-    // Prepare the SQL query
-    const char *sql = "SELECT Name, Version, Type FROM Packages";
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "SQL error: %s", zErrMsg);
-        sqlite3_free(zErrMsg);
-        return 1;
-    }
+  // Prepare the SQL query
+  const char *sql = "SELECT Name, Version, Type FROM Packages";
+  rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+  if (rc != SQLITE_OK) {
+    msg(ERROR, "SQL error: %s", zErrMsg);
+    sqlite3_free(zErrMsg);
+    return 1;
+  }
 
-    // Execute the SQL query
-    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        msg(INFO, "%s ==> %s - %s", sqlite3_column_text(stmt, 0), sqlite3_column_text(stmt, 1), sqlite3_column_text(stmt, 2));
-    }
+  // Execute the SQL query
+  while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+    msg(INFO, "%s ==> %s - %s", sqlite3_column_text(stmt, 0),
+        sqlite3_column_text(stmt, 1), sqlite3_column_text(stmt, 2));
+  }
 
-    // Check if the SQL query was successful
-    if (rc != SQLITE_DONE) {
-        fprintf(stderr, "SQL error: %s", zErrMsg);
-        sqlite3_free(zErrMsg);
-        return -1;
-    }
+  // Check if the SQL query was successful
+  if (rc != SQLITE_DONE) {
+    fprintf(stderr, "SQL error: %s", zErrMsg);
+    sqlite3_free(zErrMsg);
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
