678a2d5c915cedb15302bb95f5be9494b1c40076
Linus Torvalds <me@theholytachanka.com>
2023-10-30 16:00:12 +0200

    Format data.h

--- include/data.h
+++ include/data.h
@@ -1,17 +1,16 @@
-#include <sqlite3.h>
 #include "libspm.h"
+#include <sqlite3.h>
 
 // create the database and the table
-int connect_db(sqlite3 **db,char* DB_PATH);
+int connect_db(sqlite3 **db, char *DB_PATH);
 int create_table_installed(sqlite3 *db);
 
-int retrieve_data_installed (sqlite3 *db, struct package *pkg,int* as_dep);
-int store_data_installed (sqlite3 *db, struct package *pkg,int as_dep);
-int remove_data_installed (sqlite3* db,char* name);
+int retrieve_data_installed(sqlite3 *db, struct package *pkg, int *as_dep);
+int store_data_installed(sqlite3 *db, struct package *pkg, int as_dep);
+int remove_data_installed(sqlite3 *db, char *name);
 
-int retrieve_data_repo(sqlite3 *db, struct package *pkg,char** format,char** section);
+int retrieve_data_repo(sqlite3 *db, struct package *pkg, char **format,
+                       char **section);
 
 // list everything in the database
 int print_all_data(sqlite3 *db);
-
-
