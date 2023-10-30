3be429cba54340e712cebb1f80f722cc2c29dfda
Linus Torvalds <me@theholytachanka.com>
2023-10-30 15:59:19 +0200

    Format version.c

--- src/version.c
+++ src/version.c
@@ -1,7 +1,4 @@
 #include "libspm.h"
 
 // A function to retrieve the version number of the libspm library.
-float version()
-{
-    return LIBSPM_VERSION;
-}
+float version() { return LIBSPM_VERSION; }
