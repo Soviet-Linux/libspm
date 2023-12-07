992d876ba102834c2d4e463c3d3d5bad46fdfbeb
AleksArt <AleksArt000@gmail.com>
2023-12-07 23:42:19 +0100

    rewriting the link function to be more readable

--- src/link.c
+++ src/link.c
@@ -34,32 +34,35 @@ void create_links(char build_loc[PATH_MAX], char dest_loc[PATH_MAX])
     // Log the count of retrieved locations for debugging
     dbg(2, "Got %d locations", count);
 
+    //this is a variable that stores the command
     char* link_cmd = calloc(count * (PATH_MAX + 64) + 1, sizeof(char));
     for(int i = 0; i < count; i++)
     {
+        //allcoaion for variable that store the command for the link
         char* read_link_cmd = calloc(PATH_MAX + 64, sizeof(char));
         char* find_cmd = calloc(PATH_MAX + 64, sizeof(char));
+        //reads the file linked by the link
         sprintf(read_link_cmd, "readlink %s/%s", build_loc, links[i]);
-
+        //searches for that file in the build directory
         sprintf(find_cmd, "( cd %s && find . -name %s ", build_loc, exec(read_link_cmd));
         find_cmd[strcspn(find_cmd, "\n")] = 0;
-
+        //creates adds the second half of the command to search for the file
         char* tmp_1 = calloc(PATH_MAX + 64, sizeof(char));
         sprintf(tmp_1, " | cut -c2- ) ");
         strcat(find_cmd, tmp_1);
-
+        //executes the search command to find the target
         char* target = calloc(PATH_MAX + 64, sizeof(char));
         sprintf(target, exec(find_cmd));
         target[strcspn(target, "\n")] = 0;
-
+        //a command to link the link to the target
         char* tmp = calloc(PATH_MAX + 64, sizeof(char));
         sprintf(link_cmd, "ln -sfv %s %s", target, links[i]);
-
+        //will copy the file start to file end in order to cerate the link in the new dir
         char* start = calloc(PATH_MAX + 64, sizeof(char));
         char* end = calloc(PATH_MAX + 64, sizeof(char));
         sprintf(start, "%s/%s", build_loc, target);
         sprintf(end, "%s/%s", dest_loc, target);
-
+        //IMPORTANT: will need to store the targets in an array and go through them, moving them to avoid overwrite
         char* copy_cmd = calloc(PATH_MAX + 64, sizeof(char));
         sprintf(copy_cmd, "cp %s %s", start, end);
 
@@ -80,7 +83,7 @@ void create_links(char build_loc[PATH_MAX], char dest_loc[PATH_MAX])
                 msg(FATAL, "Terminating the program");
             }
         }
-
+        //executes the link command
         dbg(2, "executing %s now", link_cmd);
 
         system(link_cmd);
