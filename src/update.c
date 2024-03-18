aae70ba44780708e6998714a3c862a039452a16d
AleksArt000 <aleksart000@gmail.com>
2024-03-18 18:35:14 +0100

    now it compiles

--- src/update.c
+++ src/update.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include "sqlite3.h"      // SQLite database library
-
-#include "data.h"
 
 // Include necessary headers
 #include "libspm.h"
@@ -17,51 +14,22 @@ int update()
 {
     msg(INFO, "fetching updates");
     
-    sqlite3_stmt *stmt;
-    char *zErrMsg = 0;
-    int rc;
+    getenv("SOVIET_SPM_DIR");
     int new_version_found = 0;
 
-    // Prepare the SQL query
-    const char *sql = "SELECT Name, Version FROM Packages";
-    rc = sqlite3_prepare_v2(INSTALLED_DB, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "SQL error: %s", zErrMsg);
-        sqlite3_free(zErrMsg);
-        return 1;
-    }
-    
-    // Execute the SQL query
-    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        struct package* local = calloc(1, sizeof(struct package));
-        struct package* remote = calloc(1, sizeof(struct package));
-        local->name = (char*)sqlite3_column_text(stmt, 0);
-        local->version = (char*)sqlite3_column_text(stmt, 1);
-        dbg(1, "don't ask why this is here");
-        remote->name = local->name;
-        retrieve_data_repo(ALL_DB, remote, NULL, NULL);
-        if(remote->version == NULL)
-        {
-           msg(ERROR, "No package %s exists in repo", local->name);
-        }
-        else
-        {
-            if(strcmp(local->version, remote->version) != 0)
-            {
-                 msg(INFO, "package %s is at version %s, available version is %s", local->name, local->version, remote->version);
-                 new_version_found = 1;
-            }
-        }
-        free(local);
-        free(remote);
-    }
 
-    // Check if the SQL query was successful
-    if (rc != SQLITE_DONE) {
-        msg(ERROR, "SQL error: %s", sqlite3_errmsg(INSTALLED_DB));
-        sqlite3_free(zErrMsg);
-        return -1;
+    //TODO actually get the versions
+
+    struct package* local = calloc(1, sizeof(struct package));
+    struct package* remote = calloc(1, sizeof(struct package));
+    if(strcmp(local->version, remote->version) != 0)
+    {
+            msg(INFO, "package %s is at version %s, available version is %s", local->name, local->version, remote->version);
+            new_version_found = 1;
     }
+    free(local);
+    free(remote);
+
     if(new_version_found != 0)
     {
         msg(WARNING, "new version found for one or more packages, use --upgrade to upgrade");
@@ -77,60 +45,35 @@ int update()
 int upgrade()
 {
     msg(INFO, "upgrading");
-    
-    sqlite3_stmt *stmt;
-    char *zErrMsg = 0;
-    int rc;
+
+    msg(INFO, "fetching updates");
+
+    getenv("SOVIET_SPM_DIR");
     int new_version_installed = 0;
 
-    // Prepare the SQL query
-    const char *sql = "SELECT Name, Version FROM Packages";
-    rc = sqlite3_prepare_v2(INSTALLED_DB, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        msg(ERROR, "SQL error: %s", zErrMsg);
-        sqlite3_free(zErrMsg);
+
+    //TODO actually get the versions
+
+    struct package* local = calloc(1, sizeof(struct package));
+    struct package* remote = calloc(1, sizeof(struct package));
+    if(strcmp(local->version, remote->version) != 0)
+    {
+        msg(INFO, "upgrading %s from %s to %s", local->name, local->version, remote->version);
+        uninstall(local->name);
+        char* repo = get(local, local->name);
+
+        if (repo == NULL) {
+            msg(ERROR, "Failed to download package %s", local->name);
         return 1;
-    }
-
-    // Execute the SQL query
-    while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
-        struct package* local = calloc(1, sizeof(struct package));
-        struct package* remote = calloc(1, sizeof(struct package));
-        local->name = (char*)sqlite3_column_text(stmt, 0);
-        local->version = (char*)sqlite3_column_text(stmt, 1);
-        dbg(1, "don't ask why this is here");
-        remote->name = local->name;
-        retrieve_data_repo(ALL_DB, remote, NULL, NULL);
-        if(remote->version == NULL)
-        {
-           msg(ERROR, "No package %s exists in repo", local->name);
         }
-        else
-        {
-            if(strcmp(local->version, remote->version) != 0)
-            {
-                msg(INFO, "upgrading %s from %s to %s", local->name, local->version, remote->version);
-                uninstall(local->name);
-                char* format = get(local, local->name);
 
-                if (format == NULL) {
-                    msg(ERROR, "Failed to download package %s", local->name);
-                return 1;
-                }
-
-                f_install_package_source(local->name, 0, format);
-                new_version_installed = 1;
-            }
-        }
-        free(local);
-        free(remote);
+        f_install_package_source(local->name, 0, repo);
+        new_version_installed = 1;
     }
+    free(local);
+    free(remote);
 
-    // Check if the SQL query was successful
-    if (rc != SQLITE_DONE) {
-        msg(ERROR, "SQL error: %s", sqlite3_errmsg(INSTALLED_DB));
-        return -1;
-    }
+
     if(new_version_installed == 0)
     {
         msg(WARNING, "all packages are up to date");
