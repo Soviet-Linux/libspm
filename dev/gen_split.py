76bac170f2e5a47aa8b4577ed56862ec65f87527
pkd <pkd@unsuspicious.org>
2024-07-22 06:04:12 +0200

    finix double comma test problem

--- dev/gen_split.py
+++ dev/gen_split.py
@@ -11,7 +11,14 @@ file = sys.argv[1]
 length = int(sys.argv[2])
 
 # generate random string with ASCII chars, digits, and commas of length sys.argv[2]
-random_string = ''.join(random.choice(string.ascii_letters + string.digits + ',') for _ in range(length))
+random_string = ''
+last_char = ''
+for _ in range(length):
+    next_char = random.choice(string.ascii_letters + string.digits + ',')
+    while last_char == ',' and next_char == ',':
+        next_char = random.choice(string.ascii_letters + string.digits + ',')
+    random_string += next_char
+    last_char = next_char
 
 # write it to file
 with open(file, 'w') as f:
